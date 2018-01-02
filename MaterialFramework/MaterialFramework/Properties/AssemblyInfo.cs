ce9fa0f77339d4208007d6f8fe9fddfc18b5718d
Alexander Ritter <braydelritter@gmail.com>
2018-01-02 13:36:06 -0600

    Changed the version to 4.0.4

--- MaterialFramework/MaterialFramework/Properties/AssemblyInfo.cs
+++ MaterialFramework/MaterialFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vortex Studio")]
 [assembly: AssemblyProduct("MaterialFramework")]
-[assembly: AssemblyCopyright("Copyright © Vortex Studio 2015 - 2016, Indie Goat 2016 - 2017")]
+[assembly: AssemblyCopyright("Copyright © Vortex Studio 2015 - 2016, Indie Goat 2016 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.3.0")]
-[assembly: AssemblyFileVersion("4.0.3.0")]
+[assembly: AssemblyVersion("4.0.4.0")]
+[assembly: AssemblyFileVersion("4.0.4.0")]
