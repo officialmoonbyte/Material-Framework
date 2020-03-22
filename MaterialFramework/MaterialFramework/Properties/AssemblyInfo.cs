f8570c0a2fcfaa865ee9b872baac2558034f994a
braydel <alexritter@moonbyte.net>
2020-03-22 17:07:56 -0500

    Update from 5.1.4

--- MaterialFramework/MaterialFramework/Properties/AssemblyInfo.cs
+++ MaterialFramework/MaterialFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Moonbyte")]
 [assembly: AssemblyProduct("MaterialFramework")]
-[assembly: AssemblyCopyright("Copyright © Moonbyte Corporation 2015 - 2019")]
+[assembly: AssemblyCopyright("Copyright © Moonbyte Corporation 2015 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.3.0")]
-[assembly: AssemblyFileVersion("5.1.3.0")]
+[assembly: AssemblyVersion("5.1.4.0")]
+[assembly: AssemblyFileVersion("5.1.4.0")]
